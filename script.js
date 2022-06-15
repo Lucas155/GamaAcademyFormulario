ccec52ea36df1ec1fc0b212acd986572db60f587
lucasdev155 <63871665+lucasdev155@users.noreply.github.com>
2022-06-15 18:18:16 -0300

    Formulario

---
+++ script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+  $('button').click(function(){
+    // $('#test').hide();
+
+    // ajax api
+    // $.ajax({
+    //   url: 'https://webhook.site/a1857554-92e4-4317-a949-9712b910ba4b',
+    //   type: 'POST',
+    //   data: {
+    //     name: 'Douglas'
+    //   },
+    //   contentType: 'application/json',
+    //   success: function() {
+    //     console.log('Sucesso!')
+    //   }
+    // })
+
+    // adicionando classe
+    // $('p').addClass('red')
+
+    // removendo classe
+    $('p').removeClass('red')
+  })
+  $('#formulario').submit(function(e){
+    e.preventDefault();
+    let data = {
+      name: $('#name').val(),
+      phone: $('#phone').val(),
+      email: $('#email').val(),
+    }
+
+    let badyContent = JSON.stringify(data)
+    $.ajax({
+      url: 'https://webhook.site/a1857554-92e4-4317-a949-9712b910ba4b',
+      type: 'POST',
+      data: badyContent,
+      contentType: 'application/json',
+      success: function(){
+        console.log('Nome: ' + data.name)
+        console.log('Telefone: ' + data.phone)
+        console.log('Email: ' + data.email)
+        alert('Os dados foram enviados' + '\n' + 'Nome: ' + data.name  + "\n" + 'Telefone: ' + data.phone + "\n" +'Email: ' + data.email)
+      }
+    })
+
+  })
+})
